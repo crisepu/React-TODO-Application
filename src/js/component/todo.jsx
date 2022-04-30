5df3795823145150aa1875357db942f0d69de21d
crisepu <csepulvedavenegas@gmail.com>
2022-04-30 14:40:07 +0000

    avance, corregir component todo

--- src/js/component/todo.jsx
+++ src/js/component/todo.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 
 const Todo = (props) => {
-	props.todo.map((todo, index) => {
-		return (
-			<li key={index} className="list-group-item border-0">
-				{todo}
-			</li>
-		);
-	});
+	!!props.todo &&
+		props.todo.map((todo, index) => {
+			return (
+				<li key={index} className="list-group-item border-0">
+					{todo}
+				</li>
+			);
+		});
 };
 
 export default Todo;
